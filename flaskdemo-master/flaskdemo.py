d71c620dc64288d4030e1d616d4ae4f0f1d966ef
jc488835 <chunjie.chen@my.jcu.edu.au>
2020-01-25 11:00:00 +0800

    Initial commit

---
+++ flaskdemo-master/flaskdemo.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import wikipedia
+
+app = Flask(__name__)
+# Set the secret key. Keep this really secret:
+app.secret_key = 'IT@JCUA0Zr98j/3yXa R~XHH!jmN]LWX/,?RT'
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/about')
+def about():
+    return "This is a new about"
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        session['search_term'] = request.form['search']
+        return redirect(url_for('results'))
+    return render_template("search.html")
+
+
+@app.route('/results')
+@app.route('/results/<my_title>')
+def results(my_title=""):
+    search_term = session[my_title]
+    page = get_page(search_term)
+    return render_template("results.html", page=page)
+
+
+def get_page(search_term):
+    try:
+        page = wikipedia.page(search_term)
+    except wikipedia.exceptions.PageError:
+        # no such page, return a random one
+        page = wikipedia.page(wikipedia.random())
+    except wikipedia.exceptions.DisambiguationError:
+        # this is a disambiguation page, get the first real page (close enough)
+        page_titles = wikipedia.search(search_term)
+        # sometimes the next page has the same name (different caps), so don't try the same again
+        if page_titles[1].lower() == page_titles[0].lower():
+            title = page_titles[2]
+        else:
+            title = page_titles[1]
+        page = get_page(wikipedia.page(title))
+    return page
+
+
+if __name__ == '__main__':
+    app.run()
